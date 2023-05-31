39951bb327cc5b328a2d25c6e76c53d431b0b4dd
Othman Nassik <othmannassik1@gmail.com>
2023-05-31 17:37:44 +0100

    Add Method for Searching customers

--- src/main/java/ma/emsi/digitalbankbackend/repositories/CustomerRepository.java
+++ src/main/java/ma/emsi/digitalbankbackend/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package ma.emsi.digitalbankbackend.repositories;
 import ma.emsi.digitalbankbackend.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByNameContains(String keyword);
 }
--- src/main/java/ma/emsi/digitalbankbackend/web/CustomerRestController.java
+++ src/main/java/ma/emsi/digitalbankbackend/web/CustomerRestController.java
@@ -17,6 +17,10 @@ public class CustomerRestController {
     public List<CustomerDTO> customers(){
         return bankAccountService.listCustomers();
     }
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword", defaultValue = "") String keyword){
+        return bankAccountService.searchCustomers(keyword);
+    }
     @GetMapping("/customers/{id}")
     public CustomerDTO getCustomer(@PathVariable Long id) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(id);
